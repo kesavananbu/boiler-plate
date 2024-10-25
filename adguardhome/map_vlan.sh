c171b1aec7d0b03df96ad91c1572ec05676ed67e
Kesavan S <36448723+kesavananbu@users.noreply.github.com>
2024-10-25 21:14:02 +0530

    Update map_vlan.sh

--- adguardhome/map_vlan.sh
+++ adguardhome/map_vlan.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#First 7 lines only for the initial run
+#sudo ip link add dvlan_bridge link eth0 type macvlan mode bridge
+#sudo ip addr add 192.168.1.100/24 dev dvlan_bridge
+#sudo ifconfig dvlan_bridge up
+#sudo ip link set dvlan_bridge up
+#sudo ip route add 192.168.1.0/24 dev dvlan_bridge
+#sudo ip route delete 192.168.1.0/24 dev eth0
+#sudo ip route add 192.168.1.0/24 dev dvlan_bridge
+
+
 # Define variables for interface and IP configurations
 PARENT_INTERFACE="eth0"                   # The physical interface on your NAS
 MACVLAN_INTERFACE="macvlan_host"          # Name for the macvlan interface
